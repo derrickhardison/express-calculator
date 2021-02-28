137d5e8cdbb23c06edf02c3313952612721086fa
dlhardi <derricklhardison@gmail.com>
2021-02-27 20:25:16 -0500

    Added routes for bmiCalulator html page

--- calculator.js
+++ calculator.js
@@ -12,9 +12,24 @@ app.post("/", (req, res) => {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
 
-  var result = num1 + num2
+  var result = num1 + num2;
 
   res.send("The result of the calculation is " + result);
+});
+
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", (req, res) => {
+
+  var userHeight = parseFloat(req.body.userHeight);
+  var userWeight = parseFloat(req.body.userWeight);
+
+  var bmi = userWeight / (userHeight * userHeight)
+
+  
+  res.send("Your bmi is " + bmi);
 });
 
 app.listen(3000, () => {
